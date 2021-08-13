68f230a60816d380866083ccc8e545da72d46622
soupjian <soupjian@163.com>
2021-08-13 15:51:30 +0800

    feat: 去除多列布局

--- src/views/Home/components/Image.vue
+++ src/views/Home/components/Image.vue
@@ -1,92 +1,91 @@
 <template>
   <div class="image-wrap">
-    <el-card shadow="never">
+    <div
+      class="item"
+      v-for="item of imgList"
+      :key="item.id"
+    >
       <img
-        src="../../../assets/background.jpg"
-        width="200"
+        :src="item.src"
         alt=""
       >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
-    <el-card shadow="never">
-      <img
-        src="../../../assets/background.jpg"
-        width="200"
-        alt=""
-      >
-    </el-card>
+      <div class="des">
+        {{item.des}}
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return {};
+    const imgList = ref<any>([
+      {
+        id: 1,
+        src: "https://cdn.pixabay.com/photo/2019/03/07/13/00/clouds-4040132__340.jpg",
+        des: "123",
+      },
+      {
+        id: 2,
+        src: "https://cdn.pixabay.com/photo/2021/06/07/22/42/tulips-6319208__340.jpg",
+        des: "234",
+      },
+      {
+        id: 3,
+        src: "https://cdn.pixabay.com/photo/2021/07/29/20/47/church-6508068__340.jpg",
+        des: "米搜狐",
+      },
+      {
+        id: 3,
+        src: "https://cdn.pixabay.com/photo/2021/08/04/15/38/mountains-6522018__340.jpg",
+        des: "hhh",
+      },
+      {
+        id: 4,
+        src: "https://cdn.pixabay.com/photo/2021/07/25/12/04/monkey-6491669__340.jpg",
+        des: "d",
+      },
+      {
+        id: 5,
+        src: "//icon.qiantucdn.com/static/images/logo-new.png",
+        des: "ashd",
+      },
+    ]);
+    return {
+      imgList,
+    };
   },
 });
 </script>
 <style lang="scss" scoped>
-:deep(.el-card) {
-  border: 0;
-  margin-bottom: 10px;
-  background: transparent;
-  text-align: center;
-  .el-card__body {
-    padding: 0 !important;
-  }
-}
 .image-wrap {
-  -moz-column-count: 3; /* 火狐 */
-  -webkit-column-count: 3; /* Safari 和 谷歌 */
-  column-count: 3;
-  // -moz-column-gap: 10px;
-  // -webkit-column-gap: 10px;
-  // column-gap: 10px;
-  margin: 0 auto;
-}
-@media screen and (max-width: 900px) {
-  .image-wrap {
-    column-count: 2; // two columns on larger phones
-  }
-}
-@media screen and (max-width: 500px) {
-  .image-wrap {
-    column-count: 1; // two columns on larger phones
+  padding: 0 20px 0 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  .item {
+    position: relative;
+    margin-left: 10px;
+    margin-bottom: 10px;
+    &:hover {
+      .des {
+        opacity: 1;
+      }
+    }
+    img {
+      width: 100%;
+    }
+    .des {
+      position: absolute;
+      bottom: 0;
+      padding: 10px 10px 8px 6px;
+      color: #fff;
+      opacity: 0;
+      transition: 0.25s;
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 0, transparent);
+    }
   }
 }
 </style>
